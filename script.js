d4e15d574b501c71a8972e04410d75dbb3307c47
Nastya <08322422nastya@gmail.com>
2020-03-06 14:46:34 +0200

    ОБЯЗАТЕЛЬНОЕ ЗАДАНИЕ 2 урок

--- script.js
+++ script.js
@@ -4,9 +4,35 @@ const hit = document.getElementById('hit'); // ссылка на span
 const dead = document.getElementById('dead'); // ссылка на span
 const enemy = document.getElementById('enemy'); // ссылка на табл или игровое поле
 const again = document.getElementById('again'); // ссылка на кнопку
+const header = document.querySelector('.header');
+
+
+const game = {
+    ships: [
+        {
+            location: ['26', '36', '46', '56'],
+            hit: ['', '', '', '']
+        }, 
+        {
+            location: ['11', '12', '13'],
+            hit: ['', '', '']
+        }, 
+        {
+            location: ['69', '79'],
+            hit: ['', '']
+        }, 
+        {
+            location: ['32'],
+            hit: ['']
+        }
+    ],
+    shipCount: 4,
+}
+
+
 
 const play = {
-    recvord: 0,
+    record: localStorage.getItem('seaBattleRecord') || 0,
     shot: 0,
     hit: 0,
     dead: 0,
@@ -15,26 +41,27 @@ const play = {
         this.render();
     },
     render() {
-        record.textContent = this.recvord;
+        record.textContent = this.record;
         shot.textContent = this.shot;
         hit.textContent = this.hit;
         dead.textContent = this.dead;
     }
 }; 
 
+
 // объект с методами на отображение реакции - попали, промазали, убили
 const show = {
-    hit() {
-
+    hit(elem) {
+        this.changeClass(elem, 'hit');
     },
     miss(elem) {
         this.changeClass(elem, 'miss');
     },
-    dead() {
-
+    dead(elem) {
+        this.changeClass(elem, 'dead');
     },
     changeClass(elem, value) {
-        elem.className = value;
+        elem.className = value; // добавили класс, перезаписали с помощью метода
     }
 }
 
@@ -42,13 +69,51 @@ const show = {
 
 const fire = (event) => {
     const target = event.target; // сохр. элемент на который был click в переменную
+    if (target.classList.length !== 0 || target.tagName !== 'TD') return; // показ. классы элемента classList
     show.miss(target); // передаем в функцию элемент
     play.updateData = 'shot';
+
+    // с помощью цикла все корабли и все координаты ячеек, и проверять есть ли такие кординаты как у нашей ячейки
+    for(let i = 0; i < game.ships.length; i++) {
+        const ship = game.ships[i];
+        const index = ship.location.indexOf(target.id);
+        if (index >= 0) {
+            show.hit(target);
+            play.updateData = 'hit';
+            ship.hit[index] = 'x';
+            const life = ship.hit.indexOf('');
+            if (life < 0) {
+                play.updateData = 'dead';
+                for (const id of ship.location) {
+                    show.dead(document.getElementById(id));
+                }
+
+                game.shipCount -= 1;
+
+                if (game.shipCount < 1) {
+                    header.textContent = 'Игра Окончена!';
+                    header.style.color = 'red';
+
+                    if (play.shot < play.record || play.record === 0) {
+                        localStorage.setItem('seaBattleRecord', play.shot); // отдаем знаение через setItem в localStorage
+                        play.record = play.shot;
+                        play.render();
+                    }
+                }
+            }
+        }
+    }
 }; 
+
+
 
 const init = () => {
     enemy.addEventListener('click', fire);
+    play.render();
 
+    again.addEventListener('click', () => {
+        location.reload();
+    });
 }; // размещаем шпиона на игоровом поле который будет следить за событием.
 // с каждым событием создаеться объект - event (событие);
 
